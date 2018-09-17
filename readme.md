1905328d85757bb0b3e58e04076629ac4a2f9b40
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-09-17 12:30:37 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# C - Quadratic Solver
+
+## Authors
+* Steven Johnson
+* Brandon Rodriguez
+* Joshua Sziede
+
+## Description
+A console program to solve quadratic equations. See program help for more details.
---
+++ src/main.c
@@ -0,0 +1,41 @@
+/**
+ * CS 4900
+ * Quadratic Equation Solver
+ */
+
+
+/**
+ * Description:
+ *  Console program to solve quadratic equations. See program help for more details.
+ */
+
+
+/**
+ * Known Issues:
+ *
+ */
+
+
+// Import headers.
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// Constant Defines.
+
+
+// Variable Declaration.
+
+
+// Method Declaration.
+
+
+/**
+ * Program's main.
+ * Initializes and runs program.
+ */
+int main(int argc, char* argv[]) {
+
+}
