98851bff0977f8079561796b403573679a196fd5
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-10-14 16:00:51 -0400

    Create makefile "read from file" spike

---
+++ spikes/make/read_from_file.c
@@ -0,0 +1,65 @@
+/**
+ * CS 4900
+ * Make read from file spike.
+ */
+
+
+/**
+ * Description:
+ *  Spike to test using make to automatically read input from a file.
+ *  This file is mostly copied from the input_handling/std_in spike.
+ */
+
+
+// Import headers.
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+/**
+ * Program's main.
+ * Initializes and runs program.
+ */
+int main(int argc, char* argv[]) {
+    int index;
+    char* input_buffer;     // Buffer to hold user input.
+    char* buffer_position;  // Position in input buffer.
+    char* input_string;     // Instance of a full line of user input.
+    off_t read_value;       // Number of characters initially read in.
+
+    input_buffer = calloc(1, 1024);
+
+    // Read input from console.
+    // Uses buffer in case multiple lines are read in at once.
+    write(1, "Enter input: ", 14);
+    read_value = read(0, input_buffer, 1024);
+    if (read_value < 0) {
+        fprintf(stderr, "Failed to read line.\n");
+    }
+
+    // Iterate through buffer of input, until null terminator (\0) is found.
+    // For each end of line (\n), pause read in and handle current input.
+    buffer_position = input_buffer;
+    while (*buffer_position != '\0') {
+        index = 0;
+        input_string = calloc(1, 1024);
+
+        // Parse until end of line found.
+        while (*buffer_position != '\n') {
+            input_string[index] = *buffer_position;
+            ++buffer_position;
+            index++;
+        }
+        ++buffer_position;
+
+        printf("Input: %s\n", input_string);
+        free(input_string);
+    }
+    free(input_buffer);
+
+    exit(0);
+}
---
+++ spikes/make/readme.md
@@ -0,0 +1,11 @@
+
+# C - Quadratic Solver > Spikes > Make
+
+## Description
+Spikes specific to make files.
+
+## Spikes
+
+### Read From File
+An example of using make to read multiple input lines from a file.
+C code is mostly copied from the "input_handling/std_in" spike.
