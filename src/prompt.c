360c89a4b1e1b3c0927a058fd2f5b473777bdb1e
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-10-29 06:06:00 -0400

    Add missing newlines at end of files

--- src/prompt.c
+++ src/prompt.c
@@ -4,26 +4,26 @@
 
 /**
  * Prompt a user for input.
- * 
+ *
  * text: The text that will be shown to the user.
  * options: If not NULL, then is displayed just before the
  *     location where the user types.
  * size: The size of the buffer to create. At most size-1
  *     characters will be written, and the string will be
  *     null terminated. (See fgets())
- * 
+ *
  * Returns an allocated string of submitted text.
  * The caller should call free() on it when done.
- * 
+ *
  * Examples:
- * 
+ *
  * prompt_user("Do you want to quit?", "(y/N)", 2);
- * 
+ *
  *     Do you want to quit?
  *     (y/N): _
- * 
+ *
  * prompt_user("What is your name?", NULL, 255);
- * 
+ *
  *     What is your name?
  *     : _
  */
